f92383f3a63f2e8ac8dfd60237790ad3d51875e9
Steve <ycsteven.tsao@gmail.com>
2019-10-04 07:44:57 +0800

    init

---
+++ src/_nav.js
@@ -0,0 +1,33 @@
+export default {
+  items: [
+    {
+      title: true,
+      name: "開發人員",
+      class: "",
+      wrapper: {
+        element: "",
+        attributes: {}
+      }
+    },
+    {
+      name: "個人資料修改",
+      url: "/users/User",
+      icon: "icon-pencil"
+    },
+    {
+      name: "我的 App",
+      url: "/users/User",
+      icon: "icon-eye"
+    },
+    {
+      name: "Android 上傳",
+      url: "/theme/typography",
+      icon: "icon-puzzle"
+    },
+    {
+      name: "iOS 上傳",
+      url: "/theme/typography",
+      icon: "icon-star"
+    }
+  ]
+};
